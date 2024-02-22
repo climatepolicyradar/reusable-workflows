2b0295e4266ea46ef071be10be6be7e8c16148c3
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-22 09:07:33 +0000

    Fix not failing on error for run_git_auto_tag step (#77)

--- auto-tag.sh
+++ auto-tag.sh
@@ -5,8 +5,8 @@ script_folder=$(dirname "${BASH_SOURCE[0]}")
 source "${script_folder}"/funcs.sh
 
 # Get the latest Git tag.
-latest_tag=$(get_latest_tag)
-if [ -z "${latest_tag}" ]; then
+latest_tag=$(set -e get_latest_tag)
+if [[ -z "${latest_tag}" ]]; then
 	echo "No tags found. Please create first tag manually to enable auto-tagging."
 	exit 1
 fi
@@ -15,9 +15,9 @@ echo "Latest tag: ${latest_tag}"
 pr_body="$1"
 
 # Get selected versioning checkboxes.
-is_patch=$(is_patch_selected "${pr_body}")
-is_minor=$(is_minor_selected "${pr_body}")
-is_major=$(is_major_selected "${pr_body}")
+is_patch=$(set -e is_patch_selected "${pr_body}")
+is_minor=$(set -e is_minor_selected "${pr_body}")
+is_major=$(set -e is_major_selected "${pr_body}")
 
 pr_number="$2"
 
@@ -30,25 +30,25 @@ fi
 # Extract the version numbers from the tag
 version_numbers=${latest_tag#v}       # Remove the leading 'v'
 version_numbers=${version_numbers%-*} # Remove the trailing '-beta'
-major_version=$(get_major "${version_numbers}")
-minor_version=$(get_minor "${version_numbers}")
-patch_version=$(get_patch "${version_numbers}")
-maturity=$(get_maturity "${latest_tag}")
-if [ ! -z "${maturity}" ]; then
+major_version=$(set -e get_major "${version_numbers}")
+minor_version=$(set -e get_minor "${version_numbers}")
+patch_version=$(set -e get_patch "${version_numbers}")
+maturity=$(set -e get_maturity "${latest_tag}")
+if [[ -n "${maturity}" ]]; then
 	maturity="-${maturity}"
 fi
 
 # Auto-tag based on most senior version selected.
 if [[ ${is_major} == true ]]; then
-	new_major_version=$(increment "${major_version}")
+	new_major_version=$(set -e increment "${major_version}")
 	new_tag=v${new_major_version}.0.0${maturity}
 	echo "Tagging as new major version ${new_tag}..."
 elif [[ ${is_minor} == true ]]; then
-	new_minor_version=$(increment "${minor_version}")
+	new_minor_version=$(set -e increment "${minor_version}")
 	new_tag="v${major_version}.${new_minor_version}.0${maturity}"
 	echo "Tagging as new minor version ${new_tag}..."
 else
-	new_patch_version=$(increment "${patch_version}")
+	new_patch_version=$(set -e increment "${patch_version}")
 	new_tag=v${major_version}.${minor_version}.${new_patch_version}${maturity}
 	echo "Tagging as new patch ${new_tag}..."
 fi
